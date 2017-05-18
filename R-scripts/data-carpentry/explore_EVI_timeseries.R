c2fdfa661e9213b3e4f773311ec68c7ffa02114c
Andrew Latimer <amlatimer@ucdavis.edu>
2017-05-17 18:17:05 -0700

    Updated paths so that the script runs, for individual example points. Polygon-based summaries in the latter part of the script still not fixed.

--- R-scripts/data-carpentry/explore_EVI_timeseries.R
+++ R-scripts/data-carpentry/explore_EVI_timeseries.R
@@ -93,8 +93,126 @@ evi_cellvalues_by_polys <- function(polys, geotif_folder, geotif_filename, geoti
   return(evi_vals)
 }
 
+#########################################################
+# Extract and look at EVI timeseries for some example locations
+
+# Get locations for Illillouette
+locs.ill = read_GE_locs("./features/example_locations/Illillouette_conif_forest_points.txt", header=FALSE)
+# Reproject to CRS of the geotifs
+r = raster(paste(geotif_folder, geotif_filename, datestr[1], ".tif", sep=""))
+locs.ill = SpatialPoints(project(locs.ill, proj4string(r)))
+# Check
+plot(r)
+plot(locs.ill, add=TRUE, col="red")
 
 
+# Extract EVI for the point locations
+evivals_ill = extract_evi(locs.ill, geotif_folder, geotif_filename, geotif_numbers = datestr)
+
+
+# Display evi for individual pixels
+par(mfrow=c(3, 4))
+for (i in 1:10) plot(evivals_ill[,i], type="l", lwd=2, col="darkgray", ylim=c(0.5, 0.9))
+# note half are blank -- don't fall into a "forested" pixel per GEE mask
+
+# Plot average for the 10 pixels
+plot(apply(evivals_ill, 1, mean, na.rm=T), type="l", col="darkgray", lwd=2)
+# Interestingly, these Illillouette high-elev forests show increasing green-ness through the summer. Could this be a signal of temperature limitation? 
+# But appears to be overall lower EVI in the drought years. 
+
+
+# Try comparing heuristically to points from lower-elevation confer forest stands
+
+locs.shaver = read_GE_locs("./features/example_locations/Shaver_Lake_conifer_forest_points.txt", header=FALSE)
+
+# Reproject to CRS of the geotifs
+r = raster(paste(geotif_folder, geotif_filename, datestr[1], ".tif", sep=""))
+locs.shaver = SpatialPoints(project(locs.shaver, proj4string(r)))
+
+# Check
+plot(r)
+plot(locs.shaver, add=TRUE, col="red")
+
+# Extract EVI
+evivals_shaver = extract_evi(locs.shaver, geotif_folder, geotif_filename, geotif_numbers = datestr)
+
+# Plot individual pixels
+par(mfrow=c(3, 4))
+for (i in 1:10) {
+  plot(evivals_shaver[,i], type="l", lwd=2, col="darkgray", ylim=c(0.5, 0.9))
+}
+
+# Plot average
+plot(apply(evivals_shaver, 1, mean, na.rm=T),  type="l", col="red")
+
+
+# Compare the means of the Illillouette to the Shaver Lake points
+
+par(mfrow=c(1,1))
+datecols = rep("white", length(dates)); datecols[which(dates$mon %in% c(6, 7, 8, 9))] = "cyan4"
+plot(apply(evivals_ill, 1, mean, na.rm=T), type="p", col=datecols, lwd=2, ylim = c(0.5, 0.85))
+datecols = rep("white", length(dates)); datecols[which(dates$mon %in% c(6, 7, 8, 9))] = "orange3"
+points(apply(evivals_shaver, 1, mean, na.rm=T),  col=datecols, lwd=2)
+
+# Both locations show substantial greening up from spring to fall. Clearly the higher elevation sites have lower mean EVI. Hard to tell from this small sample, but it looks like the higher-elevation sites also have lower amount of greening. 
+# But during 2015-16, Shaver Lake areas crashed to below Illillouette levels. 
+
+# look at annual variation for Shaver Lake points
+plot.evi.slopes <- function(z, dates) {
+  zsub = z[z$mon %in% 6:9,]
+  m=lmer(evi~daystd + (1+daystd|year), data=zsub)
+  coefs = coef(m)$year
+  plot.new(); curve(coefs[1,1]+coefs[1,2]*x, from=0, to=1.5, ylim=c(0.5, 0.9))
+  for(i in 1:length(unique(dates$year))) curve(coefs[i,1]+coefs[i,2]*x, add=T, col=i)
+  points(evi~daystd, zsub, col="gray")
+}
+
+z = data.frame(evi=apply(evivals_shaver, 1, mean, na.rm=T),  year=dates$year, mon=dates$mon, day=dates$yday, daystd = scale(dates$yday))
+plot.evi.slopes(z, dates)
+
+z = data.frame(evi=apply(evivals_ill, 1, mean, na.rm=T), year=dates$year, mon=dates$mon, day=dates$yday, daystd = scale(dates$yday))
+plot.evi.slopes(z, dates)
+
+z = data.frame(evi=portlo_mean,  year=dates$year, mon=dates$mon, day=dates$yday, daystd = scale(dates$yday))
+plot.evi.slopes(z, dates)
+
+# High vs low mortality near Porterville S Sierras, ~6000 feet
+locs.porthi= read_GE_locs("./features/example_locations/Porterville_Hi_Mort.txt", header=F)
+locs.porthi = SpatialPoints(project(locs.porthi, proj4string(r)))
+evivals_porthi = extract_evi(locs.porthi, geotif_folder, geotif_filename, geotif_numbers = datestr)
+
+locs.portlo= read_GE_locs("./features/example_locations/Porterville_Lomort.txt", header=F)
+locs.portlo = SpatialPoints(project(locs.portlo, proj4string(r)))
+evivals_portlo = extract_evi(locs.portlo, geotif_folder, geotif_filename, geotif_numbers = datestr)
+
+
+par(mfrow=c(3, 4))
+for (i in 1:10) {
+  plot(evivals_portlo[,i], type="l", lwd=2, col="cyan3", ylim=c(0.5, 0.9))
+  plot(evivals_porthi[,i], type="l", lwd=2, col="orange2", ylim=c(0.5, 0.9))
+}
+
+## Yikes almost all these points are masked out for the entire time series!! 
+
+
+
+# Compare smoothed means across sites
+porthi_mean = apply(evivals_porthi, 1, mean, na.rm=T)
+portlo_mean = apply(evivals_portlo, 1, mean, na.rm=T)
+shaver_mean = apply(evivals_shaver, 1, mean, na.rm=T)
+ill_mean  = apply(evivals_ill, 1, mean, na.rm=T)
+plot(porthi_mean, type="l", col="red")
+lines(portlo_mean,  col="blue")
+# Not sure there are obvious differences here before 2015 -- possibly late-season browning in 2013 in the high-mortality areas. 
+
+plot(porthi_mean~portlo_mean); abline(0,1) # for these few pixels, "high" mortality pixels have less total variation than "low" mortality pixels
+
+
+
+#### Not updated from here on! Broken. (Andrew 5/17/17)
+
+
+## Next look at EVI values for all cells within some subsets of pixels 
 evi_PIPO = evi_average_by_polys(polys=v.PIPO, geotif_folder, geotif_filename, datestr)
 cols = rep("darkgreen", length(evi_PIPO))
 cols[dates$mon %in% c(1,2,3,4,5,11, 12)] = "white"
@@ -126,153 +244,9 @@ days = x*16
 plot(days, evi_PIPO_cells[x,1], type="l", ylim=c(0.5, 0.85), col="white")
 palette(tim.colors(100))
 for(i in 1:50) {
-    z = sample(1:ncol(evi_PIPO_cells), size=1)
-    cols = coef(lm(evi_PIPO_cells[x,z]~days))
-    lines(days, evi_PIPO_cells[x,z], col=max(c(30,cols[2]*16000+40)), lwd=2)
-    #abline(cols, col="lightgray")
+  z = sample(1:ncol(evi_PIPO_cells), size=1)
+  cols = coef(lm(evi_PIPO_cells[x,z]~days))
+  lines(days, evi_PIPO_cells[x,z], col=max(c(30,cols[2]*16000+40)), lwd=2)
+  #abline(cols, col="lightgray")
 }
-
-
-# Get locations for Illillouette
-locs.ill = read_GE_locs("Illillouette_conif_forest_points.txt", header=FALSE)
-# Reproject to CRS of the geotifs
-r = raster(paste(geotif_filename, datestr[1], ".tif", sep=""))
-locs.ill = SpatialPoints(project(locs.ill, proj4string(r)))
-# Check
-plot(r)
-plot(locs.ill, add=TRUE, col="red")
-
-
-# Extract EVI for the point locations
-evivals_ill = extract_evi(locs.ill, geotif_folder, geotif_filename, geotif_numbers = datestr)
-
-
-# Display evi for individual pixels
-par(mfrow=c(3, 4))
-for (i in 1:10) {
-  plot(evivals_ill[,i], type="l", lwd=2, col="darkgray", ylim=c(0.5, 0.9))
-  #lines(c(15, 15), c(0.5, 0.9), col="black")
-  #lines(c(43, 43), c(0.5, 0.9), col="black")
-}
-
-# Plot average for the 10 pixels
-plot(apply(evivals_ill, 1, mean, na.rm=T), type="l", col="darkgray", lwd=2)
-# interestingly, these Illillouette high-elev forests show increasing green-ness through the summer. Could this be a signal of temperature limitation? 
-
-
-# Try comparing heuristically to points from lower-elevation confer forest stands
-
-locs.shaver = read_GE_locs("Shaver_Lake_conifer_forest_points.txt", header=FALSE)
-
-# Reproject to CRS of the geotifs
-r = raster(paste(geotif_filename, datestr[1], ".tif", sep=""))
-locs.shaver = SpatialPoints(project(locs.shaver, proj4string(r)))
-
-# Check
-plot(r)
-plot(locs.shaver, add=TRUE, col="red")
-
-# Extract EVI
-evivals_shaver = extract_evi(locs.shaver, geotif_folder, geotif_filename, geotif_numbers = datestr)
-
-# Plot individual pixels
-par(mfrow=c(3, 4))
-for (i in 1:10) {
-  plot(evivals_shaver[,i], type="l", lwd=2, col="darkgray", ylim=c(0.5, 0.9))
-}
-
-# Plot average
-plot(apply(evivals_shaver, 1, mean, na.rm=T),  type="l", col="red")
-
-
-# compare the means of the illillouette to the shaver lake points
-
-par(mfrow=c(1,1))
-datecols = rep("white", length(dates)); datecols[which(dates$mon %in% c(6, 7, 8, 9))] = "cyan4"
-plot(apply(evivals_ill, 1, mean, na.rm=T), type="p", col=datecols, lwd=2, ylim = c(0.5, 0.85))
-datecols = rep("white", length(dates)); datecols[which(dates$mon %in% c(6, 7, 8, 9))] = "orange3"
-points(apply(evivals_shaver, 1, mean, na.rm=T),  col=datecols, lwd=2)
-
-# Both locations show substantial greening up from spring to fall. Clearly the higher elevation sites have lower mean EVI. Hard to tell from this small sample, but it looks like the higher-elevation sites also have lower amount of greening. 
-# Question: how does the pattern look during severe drought? 
-# How variable is it across years in general? 
-
-# look at annual variation for Shaver Lake points
-plot.evi.slopes <- function(z, dates) {
-  zsub = z[z$mon %in% 6:9,]
-  m=lmer(evi~daystd + (1+daystd|year), data=zsub)
-  coefs = coef(m)$year
-  plot.new(); curve(coefs[1,1]+coefs[1,2]*x, from=0, to=1.5, ylim=c(0.5, 0.9))
-  for(i in 1:length(unique(dates$year))) curve(coefs[i,1]+coefs[i,2]*x, add=T, col=i)
-  points(evi~daystd, zsub, col="gray")
-}
-
-z = data.frame(evi=apply(evivals_shaver, 1, mean, na.rm=T),  year=dates$year, mon=dates$mon, day=dates$yday, daystd = scale(dates$yday))
-plot.evi.slopes(z, dates)
-
-z = data.frame(evi=apply(evivals_ill, 1, mean, na.rm=T), year=dates$year, mon=dates$mon, day=dates$yday, daystd = scale(dates$yday))
-plot.evi.slopes(z, dates)
-
-# Annual precip for Fresno county from PRISM
-z = data.frame(evi=apply(evivals_shaver, 1, mean, na.rm=T),  year=dates$year, mon=dates$mon, day=dates$yday, daystd = scale(dates$yday))
-zsub = z[z$mon %in% 6:9,]
-m=lmer(evi~daystd + (1+daystd|year), data=zsub)
-coefs = coef(m)$year
-annualppt  = c(16.25, 13.24, 8.42, 10.35, 10.8, 14.1, 15.58, 7.02, 7.53, 8.82, 19.72, 10.67, 9.87, 3.24)
-barplot(coefs[,2], names.arg= as.character(2000:2013))
-barplot(annualppt, names.arg= as.character(2000:2013))
-plot(coefs[,2]~annualppt); cor(coefs[,2],annualppt)
-# mild positive correlation
-
-
-z = data.frame(evi=portlo_mean,  year=dates$year, mon=dates$mon, day=dates$yday, daystd = scale(dates$yday))
-plot.evi.slopes(z, dates)
-zsub = z[z$mon %in% 6:9,]
-m=lmer(evi~daystd + (1+daystd|year), data=zsub)
-coefs = coef(m)$year
-annualppt  = c(16.25, 13.24, 8.42, 10.35, 10.8, 14.1, 15.58, 7.02, 7.53, 8.82, 19.72, 10.67, 9.87, 3.24)
-par(mfrow=c(2, 1))
-barplot(coefs[,2], names.arg= as.character(2000:2013))
-barplot(annualppt, names.arg= as.character(2000:2013))
-plot(coefs[],2]~annualppt); cor(coefs[,2],annualppt)
-# no correlation
-
-# High vs low mortality near Porterville S Sierras, ~6000 feet
-locs.porthi= read_GE_locs("Porterville_Hi_Mort.txt", header=F)
-locs.porthi = SpatialPoints(project(locs.porthi, proj4string(r)))
-evivals_porthi = extract_evi(locs.porthi, geotif_folder, geotif_filename, geotif_numbers = datestr)
-
-locs.portlo= read_GE_locs("Porterville_Lomort.txt", header=F)
-locs.portlo = SpatialPoints(project(locs.portlo, proj4string(r)))
-evivals_portlo = extract_evi(locs.portlo, geotif_folder, geotif_filename, geotif_numbers = datestr)
-
-
-par(mfrow=c(3, 4))
-for (i in 1:10) {
-  plot(evivals_portlo[,i], type="l", lwd=2, col="cyan3", ylim=c(0.5, 0.9))
-  plot(evivals_porthi[,i], type="l", lwd=2, col="orange2", ylim=c(0.5, 0.9))
-}
-
-## ISSUE -- almost all these points are masked out for the entire time series!! 
-
-
-
-# Compare smoothed means across sites
-
-porthi_mean = apply(evivals_porthi, 1, mean, na.rm=T)
-portlo_mean = apply(evivals_portlo, 1, mean, na.rm=T)
-shaver_mean = apply(evivals_shaver, 1, mean, na.rm=T)
-ill_mean  = apply(evivals_ill, 1, mean, na.rm=T)
-plot(porthi_mean, type="l", col="red")
-lines(portlo_mean,  col="blue")
-
-x = 1:length(datestr) # choose which dates -- 4-14 is April-Sept
-plot(loess(porthi_mean[x]~x), type="l", lwd=2, col="red", ylim=c(0.65, 0.85), main="Smoothed EVI April-Sept 2000")
-lines(loess(portlo_mean[x]~x), type="l", lwd=2, col="blue")
-lines(loess(shaver_mean[x]~x), type="l", lwd=2, col="darkgreen")
-#lines(loess(ill_mean[1:20]~x), type="l", lwd=2, col="darkgray")
-legend("topleft", c("Shaver Lake", "Porterville Hi Mort", "Porterville Lo Mort"), lwd=rep(2, 3), col=c("darkgreen", "red", "blue") )
-
-plot(porthi_mean~portlo_mean); abline(0,1) # for these few pixels, "high" mortality pixels have less total variation than "low" mortality pixels
-
 
