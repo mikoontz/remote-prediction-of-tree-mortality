b3438090500060eca5566a7bcc2272621f4aad1b
Andrew Latimer <amlatimer@ucdavis.edu>
2017-05-22 11:38:07 -0700

    read in mortality and cover data and crop to same ssn subset

--- R-scripts/data-carpentry/check_data_in_ssn_subset.R
+++ R-scripts/data-carpentry/check_data_in_ssn_subset.R
@@ -10,6 +10,20 @@ target_cover = raster("features/sierra_nevada_250m_calveg_pipo_forest-cover_whr_
 # Read in subset polygon that defines the area where the aerial mortality data has been rasterized
 area_subset = readOGR("features/so-sierra-subset-mask/so-sierra-subset-mask.shp")
 
+# Read in areal mortality raster for a specified year
+yr = 2015
+mort = raster(paste("features/ADS-rasterized/Y", yr, "_sp122.tif", sep=""))
+
 proj4string(area_subset) == proj4string(target_cover) # same projection?
+proj4string(mort) == proj4string(target_cover)
 
 # Subset the target cover to this area 
+target_cover_sub = crop(target_cover, mort)
+plot(target_cover_sub)
+
+# Create a target cover layer for pixels with specified percent PIPO cover
+target_cover_sub$cover80 = target_cover_sub >= 80
+plot(target_cover_sub)
+
+# Evaluate 
+        
