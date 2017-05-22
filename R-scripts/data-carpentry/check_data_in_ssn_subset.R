b06df8d66c0f6060de9438d7bcc9ab06662f4b20
Andrew Latimer <amlatimer@ucdavis.edu>
2017-05-22 16:06:08 -0700

    Finish displaying the areas that have PIPO (>80% PIPO WHR type) with associated mortality levels.

--- R-scripts/data-carpentry/check_data_in_ssn_subset.R
+++ R-scripts/data-carpentry/check_data_in_ssn_subset.R
@@ -11,28 +11,41 @@ target_cover = raster("features/sierra_nevada_250m_calveg_pipo_forest-cover_whr_
 area_subset = readOGR("features/so-sierra-subset-mask/so-sierra-subset-mask.shp")
 
 # Read in areal mortality raster for a specified year
-yr = 2015
-mort = raster(paste("features/ADS-rasterized/Y", yr, "_sp122.tif", sep=""))
+
+mort2015 = raster(paste("features/ADS-rasterized/Y2015_sp122.tif", sep=""))
+mort2016 = raster(paste("features/ADS-rasterized/Y2015_sp122.tif", sep=""))
+mort = mort2015 + mort2016
 
 proj4string(area_subset) == proj4string(target_cover) # same projection?
 proj4string(mort) == proj4string(target_cover)
 
 # Subset the target cover to this area 
 target_cover_sub = crop(target_cover, mort)
-plot(target_cover_sub)
+#plot(target_cover_sub)
 
 # Create a target cover layer for pixels with specified percent PIPO cover
 target_cover_sub$cover80 = target_cover_sub >= 80
-plot(target_cover_sub)
+#plot(target_cover_sub)
 
 dim(mort)
 dim(target_cover_sub) # these are at different resolutions
 mort_resamp = resample(mort, target_cover_sub$cover80)
 
 # Evaluate 
-par(mfrow=c(1,2))
-plot(mort_resamp); plot(target_cover_sub$cover80) # doesn't look like much overlap 
+#par(mfrow=c(1,2))
+#plot(mort_resamp); plot(target_cover_sub$cover80) # doesn't look like much overlap 
 
 mort_resamp$mort_bin = as.integer(mort_resamp[[1]]>50)
-z = overlay(mort_resamp$mort_bin, target_cover_sub[[1]], fun=function(x,y){return(x*y)})
+target_cover_sub$cover80[target_cover_sub$cover80==0] = NA
+z = mask(mort_resamp[[1]], target_cover_sub$cover80)
 plot(z)
+
+
+# plot mortality levels in pixels containing >=80% PIPO WHR
+plot(target_cover, main="PIPO WHR pct cover")
+plot(area_subset, add=T)
+plot(z, main="PIPO mortality in high-PIPO areas", col=rainbow(10))
+sum(getValues(z)>0, na.rm=T) # 1435 pixels in this ssn subset that are >80% PIPO and have some mortality 
+sum(getValues(z)>30, na.rm=T) # 350, pixels in this ssn subset that are >80% PIPO and have "high" mortality 
+
+# Conclusion: there is some variability in mortality in these forests, but maybe not as much mortality as I would have expected (few cells >50 trees per 250m pixel). Need to check with Derek that this looks reasonable when compared to the actual mortality data set and its range of mortality levels. 
