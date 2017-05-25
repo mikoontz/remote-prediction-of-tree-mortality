c96b3c185bc3654af6900c1d4fe1c5774bd7336a
Andrew Latimer <amlatimer@ucdavis.edu>
2017-05-25 15:26:41 -0700

    minor changes to summaries, improvement to time series plot

--- R-scripts/data-carpentry/extract_and_summarize_EVI_SSN_subset.R
+++ R-scripts/data-carpentry/extract_and_summarize_EVI_SSN_subset.R
@@ -20,6 +20,7 @@ dates = strptime(date_codes, "%Y%m%d")
 target_cover = raster("features/sierra_nevada_250m_calveg_pipo_forest-cover_whr_type.tif")
 # load one mortality layer as a template
 mort_template = raster("features/ADS-rasterized/Y2015_sp122.tif")
+mort_2015_2016 = raster("features/ADS-rasterized/Y2015_sp122.tif") + raster("features/ADS-rasterized/Y2016_sp122.tif")
 # subset the target cover raster to the mortality area 
 target_cover_sub = crop(target_cover, mort_template)
 # load one EVI layer as a template
@@ -33,7 +34,11 @@ target_pixels[target_pixels==0] = NA # set the non-target values to NA which is 
 
 # Reproject target mask to match the EVI geotiffs (in Albers projection)
 target_pixels <- projectRaster(target_pixels, evi_template)
+mort
 # ISSUE- this changes the extent to the extent of the whole evi raster
+
+# Also reproject mortality data to match the EVI geotifs
+mort_albers = projectRaster(mort_2015_2016, evi_template)
 
 # Function to extract time series of EVI values for pixels identified in the target_pixels layer
 # extracts from the set of geotifs in geotif_folder with filename starting with geotif_filename and ending in an integer date code, as specified in geotif_date_codes.
@@ -84,7 +89,9 @@ barplot(obs_by_mon, names.arg=as.character(1:12))
 # temporally mask out all months but May-Sept
 # and the the years after 2012 (i.e. the drought years)
 # Q should we include the "early" drought years of 2013-14?
-time_index = which(dates$mon %in% c(4,5,6,7,8) & dates$year<=112)
+startyear = 2000
+endyear = 2012
+time_index = which(dates$mon %in% c(4,5,6,7,8) & dates$year <= (endyear-1900) & dates$year >= (startyear-1900))
 plot(evi_mat[10,time_index])
 
 # Check how many pixels have EVI data at all 
@@ -100,79 +107,55 @@ evi_mat = evi_mat[missing_index>0,]
 
 evi_summary = data.frame(cell_number=as.integer(rownames(evi_mat)))
 evi_summary$evi_mean = apply(evi_mat[,time_index], 1, mean, na.rm=T)
-evi_summary$evi_mayjun = apply(evi_mat[,dates$mon %in% c(4, 5) & dates$year <= 112], 1, mean, na.rm=T)
-evi_summary$evi_sept = apply(evi_mat[,dates$mon == 9 & dates$year <= 112], 1, mean, na.rm=T)
+evi_summary$evi_mayjun = apply(evi_mat[,dates$mon %in% c(4, 5) & dates$year <= (endyear-1900) & dates$year >= (startyear-1900) ], 1, mean, na.rm=T)
+evi_summary$evi_sept = apply(evi_mat[,dates$mon == 9 & dates$year <= (endyear-1900) & dates$year >= (startyear-1900) ], 1, mean, na.rm=T)
 evi_summary$seas_change = evi_summary$evi_sept - evi_summary$evi_mayjun
 evi_summary$seas_change_prop = (evi_summary$evi_sept/evi_summary$evi_may)-1
+evi_summary$total_var = apply(evi_mat[,time_index], 1, var, na.rm=T)
 
 # add trends
 linear_time = scale(as.integer(dates[1:length(dates)]-dates[1]))
 for (i in 1:nrow(evi_mat)) evi_summary$linear_trend[i] = coef(lm(evi_mat[i,time_index]~linear_time[time_index]))[2]
 
-# within-year variance
-evi_summary_
 
-# within-year CV
+# add the mortality data 
+### NEED TO CHECK WHETHER THIS IS GETTING THE RIGHT PIXELS FROM MORTALITY DATA! 
+mort_masked = mask(mort_albers, target_pixels)
+evi_summary$mort = getValues(mort_masked)[as.integer(rownames(evi_mat))]
+
+
+
+
+# within-year variance
+years = 100:112
+ncells = nrow(evi_mat)
+annual.mean = matrix(NA, nrow=ncells, ncol=length(years))
+annual.var = matrix(NA, nrow=ncells, ncol=length(years))
+for (i in 1:length(years)) {
+    timeind = dates$year == years[i] & dates$mon %in% c(5,6,7,8,9)
+  for (j in 1:ncells) {
+    annual.mean[j,i] = mean(evi_mat[j,timeind], na.rm=T)
+    annual.var[j,i] = var(evi_mat[j,timeind], na.rm=T)
+  }
+}
+
+# within-year variance
+evi_summary$within_year_var = apply(annual.var, 1, mean, na.rm=T)
 
 # among-year variance
+evi_summary$among_year_var = apply(annual.mean, 1, var, na.rm=T)
 
-# among-year CV
+
+### Quick look 
+pairs(evi_summary)
+
 
 # ratio of among- to within-year variance 
 
 
+
+# look at random individual pixels
 par(mfrow=c(4,4), mar=rep(2, 4))
-for (i in 1:16) plot(evi_mat[i,]~linear_time, type="l", ylim=c(0.4, 0.9))
-
-
-###################@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# code carried over from other script below here. 
-
-# look at annual variation for Shaver Lake points
-plot.evi.slopes <- function(z, dates) {
-  zsub = z[z$mon %in% 6:9,]
-  m=lmer(evi~daystd + (1+daystd|year), data=zsub)
-  coefs = coef(m)$year
-  plot.new(); curve(coefs[1,1]+coefs[1,2]*x, from=0, to=1.5, ylim=c(0.5, 0.9))
-  for(i in 1:length(unique(dates$year))) curve(coefs[i,1]+coefs[i,2]*x, add=T, col=i)
-  points(evi~daystd, zsub, col="gray")
-}
-
-z = data.frame(evi=apply(evivals_shaver, 1, mean, na.rm=T),  year=dates$year, mon=dates$mon, day=dates$yday, daystd = scale(dates$yday))
-plot.evi.slopes(z, dates)
-
-z = data.frame(evi=apply(evivals_ill, 1, mean, na.rm=T), year=dates$year, mon=dates$mon, day=dates$yday, daystd = scale(dates$yday))
-plot.evi.slopes(z, dates)
-
-z = data.frame(evi=portlo_mean,  year=dates$year, mon=dates$mon, day=dates$yday, daystd = scale(dates$yday))
-plot.evi.slopes(z, dates)
-
-# High vs low mortality near Porterville S Sierras, ~6000 feet
-locs.porthi= read_GE_locs("./features/example_locations/Porterville_Hi_Mort.txt", header=F)
-locs.porthi = SpatialPoints(project(locs.porthi, proj4string(r)))
-evivals_porthi = extract_evi(locs.porthi, geotif_folder, geotif_filename, geotif_numbers = datestr)
-
-locs.portlo= read_GE_locs("./features/example_locations/Porterville_Lomort.txt", header=F)
-locs.portlo = SpatialPoints(project(locs.portlo, proj4string(r)))
-evivals_portlo = extract_evi(locs.portlo, geotif_folder, geotif_filename, geotif_numbers = datestr)
-
-
-par(mfrow=c(3, 4))
-for (i in 1:10) {
-  plot(evivals_portlo[,i], type="l", lwd=2, col="cyan3", ylim=c(0.5, 0.9))
-  plot(evivals_porthi[,i], type="l", lwd=2, col="orange2", ylim=c(0.5, 0.9))
-}
-
-## Yikes almost all these points are masked out for the entire time series!! 
-# Compare smoothed means across sites
-porthi_mean = apply(evivals_porthi, 1, mean, na.rm=T)
-portlo_mean = apply(evivals_portlo, 1, mean, na.rm=T)
-shaver_mean = apply(evivals_shaver, 1, mean, na.rm=T)
-ill_mean  = apply(evivals_ill, 1, mean, na.rm=T)
-plot(porthi_mean, type="l", col="red")
-lines(portlo_mean,  col="blue")
-# Not sure there are obvious differences here before 2015 -- possibly late-season browning in 2013 in the high-mortality areas. 
-
-plot(porthi_mean~portlo_mean); abline(0,1) # for these few pixels, "high" mortality pixels have less total variation than "low" mortality pixels
+for (i in 1:16) plot(evi_mat[sample(1:1203, 1),dates$mon %in% c(5,6,7,8,9)]~linear_time[dates$mon %in% c(5,6,7,8,9)], type="l", ylim=c(0.4, 0.9))
 
 
