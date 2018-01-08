befc8df33b8f4c8d481e8d7791ece9963dac7bca
Andrew Latimer <amlatimer@ucdavis.edu>
2018-01-08 09:24:38 -0800

    Fix year indexing error for summarizing climate data. Add a little EDA by merging with the evi_summary data.

--- R-scripts/data-carpentry/calculate_climate_indices.R
+++ R-scripts/data-carpentry/calculate_climate_indices.R
@@ -4,17 +4,16 @@
 # 3) cbinds these to the EVI data matrix 
 
 library(viridis)
+library(raster)
 
 load("features/working-files/climate_data_matrix_jepson_PPN+SMC_central+south.Rdata")
 
 temp_mat <- as.data.frame(clim_mat[,219:434])
 ppt_mat <- as.data.frame(clim_mat[,3:218])
-#temp_mat$month <- ppt_mat$month <- month <- rep(1:12, 17)
-#temp_mat$year <- ppt_mat$year <- rep(2000:2016, rep(12, 17))
-#temp_mat$season <- ppt_mat$season <- ifelse(month%in%c(12,1,2),"Winter", ifelse(month%in%c(3,4,5),"Spring", ifelse(month%in%c(6,7,8), "Summer",ifelse(month%in%c(9,10,11),"Fall", NA))))
 
-# Calculate indices
-years <- 2000:2016
+# Calculate simple indices
+# Annual precip (water year), and summer mean temp, winter mean temp, and annual mean temp
+years <- 1999:2016
 n.years <- length(years)
 n.cells <- nrow(clim_mat)
 ppt_wy = summer_temp = winter_temp = mean_temp = {}
@@ -30,7 +29,7 @@ ppt_wy = as.data.frame(ppt_wy)
 mean_temp = as.data.frame(mean_temp)
 winter_temp = as.data.frame(winter_temp)
 summer_temp = as.data.frame(summer_temp)
-for (i in 1:16) {
+for (i in 1:17) {
   names(ppt_wy)[i] <- paste("ppt_wy_", i+1999, sep="")
   names(mean_temp)[i] <- paste("mean_temp_", i+1999, sep="")
   names(winter_temp)[i] <- paste("winter_temp_", i+1999, sep="")
@@ -38,19 +37,41 @@ for (i in 1:16) {
 }
 
 clim_data <- cbind(clim_mat[,c("x", "y")], ppt_wy, mean_temp, winter_temp, summer_temp)
+clim_data$cell_number <- rownames(clim_data)
 head(clim_data)
 
-# Check by plotting
-coldata = clim_data$ppt_wy_2000
-cols = round(coldata-min(coldata))
-palette(viridis(max(cols)))
-plot(y~x, clim_data, col = cols)
-
-coldata = clim_data$summer_temp_2000
-cols = round(coldata-min(coldata))
-palette(heat.colors(max(cols)))
-plot(y~x, clim_data, col = cols)
 
 # Save to working files folder
 write.csv(clim_data, "features/working-files/climate_data_summaries_jepson_PPN+SMC_central+south.csv")
-     
+
+# Merge with EVI summary data for exploratory check
+load("features/working-files/evi_summary_PPN+SMC_jepson_central+south.Rdata")
+evi_template = raster("features/sierra-nevada-250m-evi-template.tif")
+
+evi_summary_clim <-  merge(evi_summary, clim_data,  by="cell_number")
+head(evi_summary_clim)
+cor(evi_summary_clim[,c(2:16, 27, 44, 61, 78)])
+heatmap(cor(evi_summary_clim[,c(2:16, 27, 44, 61, 78)]))
+
+# Quick EDA using 2008 climate data 
+cols_to_scale <- c("evi_mean", "mean_temp_2008", "ppt_wy_2008", "wet_dry_diff")
+for (i in 1:length(cols_to_scale)) evi_summary_clim[,cols_to_scale[i]] <- scale(evi_summary_clim[,cols_to_scale[i]])
+summary(lm(sqrt(mort)~evi_mean + mean_temp_2008, data=evi_summary_clim))
+summary(lm(sqrt(mort)~evi_mean * mean_temp_2008 * ppt_wy_2008 * wet_dry_diff, data=evi_summary_clim))
+
+# Check by plotting
+clim_sp = SpatialPointsDataFrame(evi_summary_clim, coords=evi_summary_clim[,c("x", "y")], proj4string = CRS(projection(evi_template)))
+coldata = clim_sp$ppt_wy_2001
+cols = round(coldata-min(coldata))
+palette(viridis(max(cols)))
+plot(clim_sp, col = cols)
+
+coldata = clim_sp$summer_temp_2000
+cols = round(coldata-min(coldata))
+palette(heat.colors(max(cols)))
+plot(clim_sp, col = cols)
+
+coldata = clim_sp$wet_dry_diff
+cols = round(coldata-min(coldata))
+palette(viridis(max(cols)))
+plot(clim_sp, col = cols)
